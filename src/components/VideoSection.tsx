4351ec49f6aea3ebd7990bdfeea90c41fae5a6eb
Samuel Jenne <samueljenne1@icloud.com>
2025-02-16 16:23:39 -0600

    Finished VideoSection styling

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -1,14 +1,16 @@
 export default function VideoSection() {
     return (
-    <section className="py-20 px-6 text-center">
-        <h2 className="text-3xl font-bold mb-4">African Inspired Interiors</h2>
-        <div className="max-w-3xl mx-auto">
-        <iframe
-            className="w-full aspect-video"
-            src="https://www.youtube.com/embed/YOUR_VIDEO_ID"
-            title="African Interiors"
-            allowFullScreen
-        ></iframe>
+    <section className="py-20 px-6 text-center bg-white">
+        <h2 className="text-9xl font-bold mb-4 text-black">African Designed</h2>
+        <div className="max-w-8xl mx-auto">
+        <video
+            className="w-full aspect-video rounded-lg"
+            src="/AfricanVideo.mp4"
+            autoPlay
+            muted
+            loop
+            playsInline
+        />
         </div>
     </section>
     );
