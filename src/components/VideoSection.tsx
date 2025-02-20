bd474084940f5c2a643bf43c12a5212d5da56e94
Samuel Jenne <samueljenne1@icloud.com>
2025-02-19 22:20:02 -0600

    Added mobile friendly video section

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -1,10 +1,10 @@
 export default function VideoSection() {
     return (
-    <section className="py-20 px-6 text-center bg-white">
-        <h2 className="text-9xl font-bold mb-4 text-black">African Designed</h2>
-        <div className="max-w-8xl mx-auto">
+    <section className="py-12 sm:py-32 px-6 text-center bg-white">
+        <h2 className="text-6xl sm:text-9xl font-bold mb-7 text-black">African Designed</h2>
+        <div className="flex justify-center">
         <video
-            className="w-full aspect-video rounded-lg"
+            className="w-full max-w-[90%] sm:max-w-8xl aspect-video rounded-lg"
             src="/AfricanVideo.mp4"
             autoPlay
             muted
