b7d03f3036f519da39a6710600f707058bc8b1d9
Samuel Jenne <samueljenne1@icloud.com>
2025-04-03 00:45:24 -0500

    Added sitemap config for deployment

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://www.sankamarketplace.com', // ✅ Change to your live domain
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    generateIndexSitemap: false,
+};
